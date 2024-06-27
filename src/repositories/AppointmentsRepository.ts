9864505553f81dae8fdb31707b6a302319c247b1
Gabriel <gabriel@iMac-de-Gabriel.mshome.net>
2024-06-27 10:33:18 -0400

    list appointments

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -7,6 +7,10 @@ class AppointmentsRepository {
 	 constructor() {
     this.appointments = [];
   }
+  
+  public all(): Appointment[] {
+    return this.appointments;
+  }
 
   public findByDate(date: Date): Appointment | null {
     const findAppointment = this.appointments.find(appointment =>
